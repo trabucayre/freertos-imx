7449d552a0aaefc7c70fd356736853a24bc1e55a
Gwenhael Goavec-Merou <gwenhael.goavec-merou@trabucayre.com>
2019-02-22 17:24:58 +0100

    suppress hardcoded ARMGCC_DIR

--- examples/imx7d_pico_m4/demo_apps/hello_world/armgcc/build_release.sh
+++ examples/imx7d_pico_m4/demo_apps/hello_world/armgcc/build_release.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-export ARMGCC_DIR=/home/gwe/misc/cortex-m3
 cmake -DCMAKE_TOOLCHAIN_FILE="../../../../../tools/cmake_toolchain_files/armgcc.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release  .
 make -j4
